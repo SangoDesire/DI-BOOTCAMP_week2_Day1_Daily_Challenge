5fc3ef9a1993c7503b28945a6fcc8f9a5aab34a5
SangoDesire <analysteprodez@gmail.com>
2023-01-11 16:31:53 +0100

    Correction de du week2_Day1_Dailychallenge

--- exo1.js
+++ exo1.js
@@ -1,17 +1,17 @@
 /*Exercise 1: */
 const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
-/*Remove Banana from the array*/
-fruits.shift(0, 1)
+/*1-Remove Banana from the array*/
+fruits.shift(0, 1);
 
-/*Sort the array in alphabetical order.*/
-fruits.sort()
+/*2-Sort the array in alphabetical order.*/
+fruits.sort();
 
 
-/*Add “Kiwi” to the end of the array.*/
-fruits.push(Kiwi)
+/*3-Add “Kiwi” to the end of the array.*/
+fruits.push('Kiwi');
 
-/*Remove “Apples” from the array. Don’t use the same method as in part 1.*/
-fruits.splice
+/*4-Remove “Apples” from the array. Don’t use the same method as in part 1.*/
+fruits.splice(0,1);
 
-/*Sort the array in reverse order. (Not alphabetical... i.e. [‘a’, ‘c’, ‘b’] becomes [‘b’, ‘c’, ‘a’])*/
-fruits.reverse()
+/*5-Sort the array in reverse order. (Not alphabetical... i.e. [‘a’, ‘c’, ‘b’] becomes [‘b’, ‘c’, ‘a’])*/
+fruits.reverse();
